bf259d9b9ff6e5e494a4b1451245d15652aee4cd
Marco Antonio Castillo Gonzales <marcocastillogonza@gmail.com>
2025-11-26 04:04:56 -0500

    update: Gemini AI Implementation

--- src/core/services/predictions/predictions.service.ts
+++ src/core/services/predictions/predictions.service.ts
@@ -23,7 +23,7 @@ export class PredictionsService {
     // 2. Configuración del Modelo "Pro"
     // Usamos gemini-1.5-pro para obtener razonamiento avanzado y mejores consejos.
     this.model = this.genAI.getGenerativeModel({ 
-      model: "gemini-1.5-flash-latest",
+      model: "gemini-2.0-flash",
       generationConfig: {
         temperature: 0.7, // Balance perfecto entre creatividad y precisión
         topK: 40,
