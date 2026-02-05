679798ffb2004b87077f62d3d3db9a4fe17c5aff
Marco Antonio Castillo Gonzales <marcocastillogonza@gmail.com>
2026-02-05 01:33:05 -0500

    remove: deleted useless files.

--- src/shared/pipes/shared.pipes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ValidateFolderPipe } from './validate-folder.pipe';
-
-@Module({
-  providers: [ValidateFolderPipe],
-  imports: [],
-  exports: [ValidateFolderPipe],
-})
-export class SharedPipesModule {}
